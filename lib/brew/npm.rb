f0a3581fb12ce906a01d23234e3ce02420a93803
motoki-shun <shuuuuuny+github@gmail.com>
2020-06-13 17:03:57 +0900

    init gem
    $ bundle gem brew-npm --no-git --bin --coc --mit

---
+++ lib/brew/npm.rb
@@ -0,0 +1,8 @@
+require "brew/npm/version"
+
+module Brew
+  module Npm
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/brew/npm/version.rb
@@ -0,0 +1,5 @@
+module Brew
+  module Npm
+    VERSION = "0.1.0"
+  end
+end
