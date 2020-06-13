7d84c537225422c856e8458ede53e57159808786
motoki-shun <shuuuuuny+github@gmail.com>
2020-06-13 19:12:25 +0900

    NODE_FLAGS

--- lib/brew/npm/cli.rb
+++ lib/brew/npm/cli.rb
@@ -17,15 +17,16 @@ module Brew::Npm::CLI
     "help"      => "This message"
   }
 
-  # HOMEBREW_RUBY_FLAG = "--homebrew-ruby"
-  # SYSTEM_RUBY_FLAG   = "--system-ruby"
-  # RUBY_FLAGS = [HOMEBREW_RUBY_FLAG, SYSTEM_RUBY_FLAG]
+  HOMEBREW_NODE_FLAG = "--homebrew-node"
+  SYSTEM_NODE_FLAG   = "--system-node"
+  NODE_FLAGS = [HOMEBREW_NODE_FLAG, SYSTEM_NODE_FLAG]
 
   class Arguments
+    attr_reader :node_flag
+
     def initialize(args)
-      # @ruby_flag          = args.select {|a| RUBY_FLAGS.include?(a) }.last
-      # @args               = args.reject {|a| RUBY_FLAGS.include?(a) }
-      @args               = args
+      @node_flag          = args.select {|a| NODE_FLAGS.include?(a) }.last
+      @args               = args.reject {|a| NODE_FLAGS.include?(a) }
       @args_without_flags = @args.reject {|a| a.start_with?("-") }
     end
 
@@ -52,15 +53,8 @@ module Brew::Npm::CLI
   end
 
   def fetch_version(name, version = nil)
-    # npm_info = `npm info "#{name}"`.lines
     npm_info = parse_json `npm info --json "#{name}"`
-
-    # unless npm_info.detect { |f| f =~ /^#{name}@([^\s,]+)\s.*/ }
-    #   abort "Could not find a valid npm '#{name}'"
-    # end
     abort "Could not find a valid npm '#{name}'" if npm_info.nil?
-
-    # version ||= $1
     version ||= npm_info["version"]
     version
   end
@@ -95,19 +89,19 @@ module Brew::Npm::CLI
     ENV["HOMEBREW_PREFIX"] || `brew --prefix`.chomp
   end
 
-  def expand_formula(name, version)
+  def expand_formula(name, version, use_homebrew_node=false)
     klass           = "Npm" + name.capitalize.gsub(/[-_.\s]([a-zA-Z0-9])/) { $1.upcase }.gsub('+', 'x')
     user_npmrc      = "#{ENV['HOME']}/.npmrc"
     template_file   = File.expand_path("../formula.rb.erb", __FILE__)
     template        = ERB.new(File.read(template_file))
-    template.result(binding)
+    template.result(binding) # TODO: avoid binding. specify variables.
   end
 
-  def with_temp_formula(name, version)
+  def with_temp_formula(name, version, use_homebrew_node)
     filename = File.join Dir.tmpdir, "npm-#{name}.rb"
 
     open(filename, "w") do |f|
-      f.puts expand_formula(name, version)
+      f.puts expand_formula(name, version, use_homebrew_node)
     end
 
     yield filename
@@ -115,14 +109,19 @@ module Brew::Npm::CLI
     File.unlink filename
   end
 
+  def homebrew_node?(node_flag)
+    File.exist?("#{homebrew_prefix}/opt/node") &&
+      node_flag.nil? || node_flag == HOMEBREW_NODE_FLAG
+  end
+
   def run(args = ARGV)
     arguments = process_args(args)
     name      = arguments.npm
     version   = fetch_version(name, arguments.supplied_version)
     # check node/npm is installed
-    pp arguments, name, version
-    return
-    with_temp_formula(name, version) do |filename|
+    # pp arguments, name, version
+    # return
+    with_temp_formula(name, version, homebrew_node?(arguments.node_flag)) do |filename|
       case arguments.command
       when "formula"
         $stdout.puts File.read(filename)
